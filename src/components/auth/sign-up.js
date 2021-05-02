74e798ad6e6de7400b2d345acbcc04a44a3d60a6
bapplewh <bapps.5@gmail.com>
2021-05-02 16:44:26 -0600

    developed sign up form and modified form button

--- src/components/auth/sign-up.js
+++ src/components/auth/sign-up.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
+import SignUpForm from "./sign-up-form";
+import PageTitle from "../page-title";
+
 class SignUp extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
             <div className="sign-up">
-                Sign up
+                <PageTitle className="sign-up__page-title" title="Register" />
+                <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
             </div>
         )
     }
 }
+
 
 export default SignUp;
