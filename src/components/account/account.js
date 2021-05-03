54a1bd86e0df6a6da715dbf57caaa153b931743d
bapplewh <bapps.5@gmail.com>
2021-05-02 20:10:09 -0600

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -9,6 +9,20 @@ import PurchaseHistory from './purchase-history';
 
 class Account extends Component {
     componentDidMount() {
+        
+        const headerLinks = [
+            {   
+                _id: 0,
+                title: "Shop",
+                path: "/shop"
+            },
+            {
+                _id: 1,
+                title: "Logout",
+                path: "/"
+            }
+        ]
+        
         const navbarLinks = [
             {   
                 _id: 0,
@@ -24,7 +38,7 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
