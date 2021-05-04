cb9336064201565a0c1fbf1323cc206c58f8bef0
bapplewh <bapps.5@gmail.com>
2021-05-03 22:16:00 -0600

    built account info form from sign in form template

---
+++ src/components/account/account-info-form.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+import { reduxForm, Field } from "redux-form";
+
+import { FormInput, FormButton } from "../form-fields";
+
+import history from  "../../history";
+import Details from "../details";
+
+class AccountInfoForm extends Component {
+    render() {
+        const { className, handleSubmit } = this.props;
+        const links = [
+            {
+                _id: 0,
+                title: 'Not registered? Create an account here',
+                onClick: () => history.push('/signup')
+            },
+            {
+                _id: 1,
+                title: 'Forgot account email',
+                onClick: () => console.log('forgot email')
+            },
+            {
+                _id: 2,
+                title: 'Forgot password',
+                onClick: () => console.log('forgot password')
+            }
+        ]
+        
+        return (
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
+                <Field 
+                    className="sign-in-form__email" 
+                    type="email" 
+                    title="Email" 
+                    placeholder="Email" 
+                    name="email" 
+                    component={FormInput} 
+                />
+
+                <Field 
+                    className="sign-in-form__password" 
+                    type="password" 
+                    title="Password" 
+                    placeholder="Password" 
+                    name="password" 
+                    component={FormInput} 
+                />
+
+                <div className="sign-in-form__line"></div>
+
+                <Field 
+                    className="sign-in-form__login" 
+                    onClick={() => history.push('/account')}
+                    type="submit" 
+                    title="Login" 
+                    name="login" 
+                    component={FormButton} 
+                />
+                <Details className='sign-in-form__details' title='Quick Links' links={links} />
+            </form>
+        )
+    }
+}
+
+AccountInfoForm = reduxForm({
+    form: "AccountInfoForm"
+})(AccountInfoForm);
+
+export default AccountInfoForm;
--- src/components/account/account-info.js
+++ src/components/account/account-info.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import PageTitle from "../page-title";
+import AccountInfoForm from  "./account-info-form";
+
 
 class AccountInfo extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                account info
+            <div className="account-info">
+                <PageTitle className="account-info" title="Account Information" />
+                <AccountInfoForm onSubmit={this.onSubmit} className="account-info__form" />
             </div>
         )
     }
