6981bd0bb7b47e0af7975a3fcd3d88b33856cb38
bapplewh <bapps.5@gmail.com>
2021-05-05 00:28:22 -0600

    refactored navbar to take additional onclick

--- src/components/header-navbar/navbar.js
+++ src/components/header-navbar/navbar.js
@@ -5,13 +5,21 @@ import * as actions from  "../../actions";
 
 
 class NavBar extends Component {
+  handleOnClick = (link) => {
+    this.props.changeNavbarActive(link._id)
+
+    if (this.props.onClick) {
+      this.props.onClick(link._id);
+    }
+  }
+
   render() {
     return (
       <div className='navbar'>
         {
           this.props.navbarLinks.map((link, index) => {
             return (
-              <a className={`navbar__link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => this.props.changeNavbarActive(link._id)}>
+              <a className={`navbar__link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => this.handleOnClick(link)}>
                 {link.title}
               </a>
             )
@@ -23,9 +31,10 @@ class NavBar extends Component {
 }
 
 function mapStateToProps(state) {
-  const { navbarLinks } = state.headerNavbar;
+  const { navbarLinks, onClick } = state.headerNavbar;
   return {
-    navbarLinks
+    navbarLinks,
+    onClick
   }
 }
 
